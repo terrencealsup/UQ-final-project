178297c42059a13fa1e4ac20bf21100cca6ba6d8
Terrence Alsup <alsup@box774.cims.nyu.edu>
2019-05-08 14:12:27 -0400

    changed MFCE toy problem

--- code/MFCE_LN_test1.m
+++ code/MFCE_LN_test1.m
@@ -1,15 +1,15 @@
-% Test case to find the probability that Z >= t where Z is standard normal.
+% Test case to find the probability that Z >= t where Z is log-normal.
 % P_p(Z >= t) = P_p(f^{(L)}(Z) >= t)
 
-N = 100;
-m = 1.6;
-s = 0.25;
+N = 2e1;
+m = 0;
+s = 1;
 
 Z = exp(m + s*randn(N,1));
 
 p = @(x) normpdf(log(x), m, s)./x;
 
-t = 25;
+t = 3000;
 rho = 0.9;
 dt = 0.01;
 
@@ -21,7 +21,7 @@ lvl = 1:L;
 
 [prob, mu, S] = MFCE_LogNormal(Z, t, p, f, lvl, N, rho, dt);
 
-fprintf("\nMFCE Estimate      = %f\n",prob);
+fprintf("\nMFCE Estimate    = %f\n",prob);
 
 prob_true = 1 - normcdf((log(t) - m) / s);
 fprintf("True Probability = %f\n", prob_true);
@@ -30,13 +30,18 @@ relerr = abs(prob_true - prob)/prob_true;
 fprintf("Relative Error   = %f\n\n", relerr);
 
 figure(1);
-x = linspace(0.5, 8, 250);
+x = linspace(0.01, 2*t, 500);
 pdf = p(x);
 plot(x, pdf, 'b-');
 hold on
 bias = normpdf(log(x), mu, sqrt(S))./x;
 plot(x, bias, 'r-');
-legend(["Target", "Biasing"], 'interpreter', 'latex', 'Location', 'NorthWest');
+
+opt_bias = (f(x, L) >= t) .* p(x) / prob_true;
+plot(x, opt_bias, 'm-');
+
+legend(["Target", "Biasing", "Optimal Biasing"], 'interpreter', 'latex', 'Location', 'NorthEast');
 xlabel('$x$', 'interpreter', 'latex');
 ylabel('Density', 'interpreter', 'latex');
 title('MFCE Biasing Density', 'interpreter', 'latex');
+hold off
