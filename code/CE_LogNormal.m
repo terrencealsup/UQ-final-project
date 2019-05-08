8d1c8d24e8354b211ea31157a8556138b38d9497
fredglaw <Law@cims.nyu.edu>
2019-05-08 19:44:57 -0400

    edits for speedup, old code commented

--- code/CE_LogNormal.m
+++ code/CE_LogNormal.m
@@ -44,6 +44,7 @@ end
 
 P  = zeros(N, 1);   % The target density evaluations.
 Q  = ones(N, 1);    % The biasing density evaluations.
+buffer = 1e-3 * eye(d);
 
 % The main iteration.
 while tk < t
@@ -72,47 +73,61 @@ while tk < t
     W = ones(N, 1);
     if k ~= 1 || ~isempty(mlvl)
         % Compute the target and biasing densities at the samples.
-        for i = 1:N
-            if I(i) ~= 0
-                P(i) = p(Z(i,:));
-                Q(i) = mvnpdf(log(Z(i,:)'), mu, S) ./ prod(Z(i,:));
-                W(i) = P(i) / Q(i);
-            else
-                W(i) = 0;
-            end
-        end
+%         for i = 1:N
+%             if I(i) ~= 0
+%                 P(i) = p(Z(i,:));
+%                 Q(i) = mvnpdf(log(Z(i,:)'), mu, S) ./ prod(Z(i,:));
+%                 W(i) = P(i) / Q(i);
+%             else
+%                 W(i) = 0;
+%             end
+%         end
+        %vectorize
+        P = p(Z);
+        Q = mvnpdf(log(Z),mu',S) ./ prod(Z,2);
+        W = P ./ Q;
     end
     
     % Estimate the mean.
-    mu = zeros(d, 1);
-    for i = 1:N
-        if I(i) ~= 0 && W(i) ~= 0
-            mu = mu + (I(i) * W(i) * log(Z(i,:)'));
-        end
-    end
-    mu = mu / (I' * W); % Re-normalize.
+%     mu = zeros(d, 1);
+%     for i = 1:N
+%         if I(i) ~= 0 && W(i) ~= 0
+%             mu = mu + (I(i) * W(i) * log(Z(i,:)'));
+%         end
+%     end
+%     mu = mu / (I' * W); % Re-normalize.
+
+    %vectorized
+    temp1 = I.*W; sum1 = sum(temp1);
+    mu = (log(Z)' * temp1) / sum1;
     
     % Estimate the covariance matrix.
     S = zeros(d);
+    S_temp = log(Z)' - mu*ones(1,N);
     for i = 1:N
         if I(i) ~= 0 && W(i) ~= 0
-            S = S + I(i) * W(i) * ((log(Z(i,:)') - mu) * (log(Z(i,:)') - mu)');
+%             S = S + I(i) * W(i) * ((log(Z(i,:)') - mu) * (log(Z(i,:)') - mu)');
+            temp2 = S_temp(:,i);
+            S = S + temp1(i) * (temp2 * temp2');
         end
     end
-    S = S / (I' * W); % Re-normalize.
+%     S = S / (I' * W); % Re-normalize.
+    S = S/sum1;
     
     % Inflate the covariance matrix so it is better conditioned.
-    [V, D] = eig(S);
-    D = max(D, 1e-3 * eye(d)); % Minimum eigenvalue is at least 10^-3.
-    S = V * D * V';
-    %S = S + 1e-3 * eye(d);
+%     [V, D] = eig(S);
+%     D = max(D, 1e-3 * eye(d)); % Minimum eigenvalue is at least 10^-3.
+%     S = V * D * V';
+%     S = S + 1e-3 * eye(d);
+      S = S + buffer;
     
     % Update iteration counter.
     k = k + 1;
 end
-
+disp(['# CE iterations is K=',num2str(k-1)]);
 % Now compute the Cross Entropy estimate for the probability.
 
-prob = (I' * W) / N;
+% prob = (I' * W) / N;
+prob = sum1 / N;
 
 end
